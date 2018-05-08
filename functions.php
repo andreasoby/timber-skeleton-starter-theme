5489e4cab2e5a1918ed9e9be40b67b8fd77783c2
Marcin Karpezo <marcin@karpezo.pl>
2018-05-08 14:41:28 +0200

    removed timber-library, better to use plugin. It gets outdated easly

--- functions.php
+++ functions.php
@@ -1,8 +1,5 @@
 <?php
 
-/* let's move timber plugin into independent theme */
-require_once(get_template_directory().'/timber-library/timber.php');
-
 if ( ! class_exists( 'Timber' ) ) {
 	add_action( 'admin_notices', function() {
 			echo '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="' . esc_url( admin_url( 'plugins.php#timber' ) ) . '">' . esc_url( admin_url( 'plugins.php' ) ) . '</a></p></div>';
